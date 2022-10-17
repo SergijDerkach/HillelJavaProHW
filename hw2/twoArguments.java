d0622403d92db750466e1d83c940e120651cfaf9
S.Derkach <sergjj-derkach@rambler.ru>
2022-10-17 21:34:42 +0300

    hw 2.1

---
+++ hw2/twoArguments.java
@@ -0,0 +1,12 @@
+package hw2;
+
+public class twoArguments {
+    public static void main(String[] args) {
+        double arguOne = 4.5d;
+        double arguTwo = 2d;
+
+        double midArithmetic =(arguOne+arguTwo)/2;
+
+        System.out.println(midArithmetic);
+    }
+}
