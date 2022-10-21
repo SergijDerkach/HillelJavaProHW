0ee2b2dc92e6914247ec216c6a296659451c5bc8
S.Derkach <sergjj-derkach@rambler.ru>
2022-10-21 12:14:01 +0300

    13 tasks

---
+++ hw3/chessboard.java
@@ -0,0 +1,26 @@
+package hw3;
+
+public class chessboard {
+    public static void main(String[] args) {
+        String [][] board = new String[8][8];
+        String white = "W";
+        String black = "B";
+
+        for(int i =0; i<board.length;i++){
+            for(int j = 0; j<board[i].length;j++){
+                if(i %2 != 0){
+                    if(j %2 != 0){
+                        board[i][j] = white;
+                    }else
+                        board[i][j] = black;
+                }else
+                    if(j %2 == 0){
+                        board[i][j] = white;
+                    }else
+                        board[i][j] = black;
+                System.out.print(" "+board[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+}
