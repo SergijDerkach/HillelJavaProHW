4cd6c8f2fb14bdb42f1e8b55c14e86ca4d557976
S.Derkach <sergjj-derkach@rambler.ru>
2022-10-21 11:25:45 +0300

    12 tasks

---
+++ hw3/mediumArr.java
@@ -0,0 +1,16 @@
+package hw3;
+
+public class mediumArr {
+    public static void main(String[] args) {
+        int nums[] = {2,5,-9,4,2,6,7};
+        double result = 0d;
+
+        for(int i = 0; i<nums.length;i++){
+            result +=nums[i];
+        }
+
+        result = result/(nums.length);
+        System.out.println("Medium number arry nums = "+String.format("%.2f",result));
+
+    }
+}
