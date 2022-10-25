fb9a3c33eef19c6ad616f8d9ea37d3cfaebc8547
S.Derkach <sergjj-derkach@rambler.ru>
2022-10-25 17:54:21 +0300

    Готовое задание

--- hw4/TripByCar.java
+++ hw4/TripByCar.java
@@ -1,45 +1,65 @@
 package hw4;
-
 import java.text.DecimalFormat;
-
 public class TripByCar {
     public static void main(String[] args) {
         DecimalFormat decimalFormat = new DecimalFormat( "#.#" );
-
-//        double tank = Integer.parseInt(args[0]);
-//        double balanceInTank = Integer.parseInt(args[1]);
-//        double fuelConsumption = Integer.parseInt(args[2]);
 
         double priceFuel = Integer.parseInt(args[0]);
         double tank = 50d;
         double balanceInTank = 35d;
         double fuelConsumption = 7d;
-
         double distKievZhashkiv = 149;
         double distZhashkivKryveOzero = 153;
         double distKryveOzeroOdessa = 179;
-        double fulldistance = distKievZhashkiv + distZhashkivKryveOzero + distKryveOzeroOdessa;
-        double fuelForDistance = 0;
+        double fuelForDistance = 0d;
 
         TripByCar car = new TripByCar();
-        balanceInTank = car.fillUpFullTank(tank);
-        System.out.println("Бак заправлен, в баке: " + balanceInTank);
+        // в киеве
+        System.out.println("В наче поездке в баке: " + balanceInTank);
+        System.out.println("-----------------------------------");
+
+        // от киев до жашкова
         balanceInTank = Double.parseDouble(decimalFormat.format(car.nowBake(balanceInTank,distKievZhashkiv,fuelConsumption)));
-        System.out.println("После проезда "+ distKievZhashkiv +" км в баке осталось: " + balanceInTank);
-        fuelForDistance = Double.parseDouble(decimalFormat.format(car.needFuel(fulldistance,fuelConsumption,balanceInTank)));
-        System.out.println("Для проезда "+fulldistance +" нужно дозаправить еще:" + fuelForDistance);
+        System.out.println("От Киев до Жашкова в баке : " + balanceInTank);
+        fuelForDistance = Double.parseDouble(decimalFormat.format(car.needFuel(distKievZhashkiv,fuelConsumption)));
+        System.out.println("От Киев до Жашкова: " + fuelForDistance);
+        balanceInTank = car.fillUpFullTank(tank);
+        System.out.println("В Жашковае заправил полный бак: " + balanceInTank);
+        System.out.println("-----------------------------------");
+
+        // от жашкова до кривого озера
+        fuelForDistance += Double.parseDouble(decimalFormat.format(car.needFuel(distZhashkivKryveOzero,fuelConsumption)));
+        System.out.println("От Киев до Кривого Озера: " + Double.parseDouble(decimalFormat.format(fuelForDistance)));
+        balanceInTank = car.fillUpFullTank(tank);
+        System.out.println("В Кривом Озере заправил полный бак: " + balanceInTank);
+        System.out.println("-----------------------------------");
+
+        // от кривого озера до одессы
+        fuelForDistance += Double.parseDouble(decimalFormat.format(car.needFuel(distKryveOzeroOdessa,fuelConsumption)));
+        System.out.println("От Кривого Озера до Одессы: " + Double.parseDouble(decimalFormat.format(fuelForDistance)));
+        System.out.println("-----------------------------------");
+
+        System.out.println("Стоимость потраченого бензина на поезду: " +Double.parseDouble(decimalFormat.format(fuelForDistance*priceFuel)));
+        System.out.println("В баке осталось: " + (balanceInTank - Double.parseDouble(decimalFormat.format(car.needFuel(distKryveOzeroOdessa,fuelConsumption)))));
+        System.out.println("-----------------------------------");
+
+        double enoughFuel = Double.parseDouble(decimalFormat.format(car.nowBake(25,380,7)));
+
+        if(enoughFuel > 0){
+            System.out.println("Бензина хватить для поездки");
+        }else
+            System.out.println("Необходимо дозаправить минимум - " + Math.abs(enoughFuel));
     }
     double fillUpFullTank(double tank){
         return tank;
     }
 
-    double nowBake(double balanceInTank, double distKievZhashkiv, double fuelConsumption){
-        return balanceInTank - (distKievZhashkiv/fuelConsumption);
+    double nowBake(double balance, double dist, double fuelConsumption){
+
+        return balance - ((dist/100)*fuelConsumption);
     }
 
-    double needFuel(double fulldistance,double fuelConsumption,double balanceInTank){
-        return Math.abs(balanceInTank - (fulldistance/fuelConsumption));
+    double needFuel(double distance,double fuelConsumption){
+        return Math.abs((distance/100)*fuelConsumption);
     }
-
-
 }
